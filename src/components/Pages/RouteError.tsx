fc00d8bca1abaca349cba6b024caec3b7d4d333b
SAY10s <nikodemmajzner@gmail.com>
2024-02-19 00:22:05 +0100

    ADD: routeError

--- src/components/Pages/RouteError.tsx
+++ src/components/Pages/RouteError.tsx
@@ -1,5 +1,20 @@
 import Navbar from "../NavBar/Navbar";
+import classes from "./RouteError.module.scss";
+import react from "../../../public/react.svg";
+import { Link } from "react-router-dom";
 
 export default function RouteError() {
-  return <Navbar />;
+  return (
+    <div className={classes.container}>
+      <Navbar />
+      <h1>
+        Strona w <span>przygotowaniu!</span>
+      </h1>
+      <img src={react} alt="" />
+      <h2>Strona, której szukasz jeszcze nie jest gotowa... </h2>
+      <Link to={""}>
+        <button className={classes.kontakt}>powrót</button>
+      </Link>
+    </div>
+  );
 }
