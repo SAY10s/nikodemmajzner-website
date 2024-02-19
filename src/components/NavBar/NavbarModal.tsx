85bc289cf2c03822db78f38b2c5be27e454d8858
say10s <nikodemmajzner@gmail.com>
2024-02-19 13:39:26 +0100

    ADD: btn wrapper in modal

--- src/components/NavBar/NavbarModal.tsx
+++ src/components/NavBar/NavbarModal.tsx
@@ -1,5 +1,5 @@
 import { forwardRef } from "react";
-// import Button from "./Button";
+import Button from "./Button";
 
 export type Ref = HTMLDialogElement;
 import classes from "./NavbarModal.module.scss";
@@ -11,6 +11,13 @@ const NavbarModal = forwardRef<Ref>(function NavbarModal(props, ref) {
         <section className={classes.navLogo}>
           <div>Nikodem</div>
           <div>Majzner</div>
+        </section>
+        <section className={classes.btnWrapper}>
+          <Button link="omnie">o mnie</Button>
+          <Button link="kontakt">kontakt</Button>
+          <Button link="https://github.com/SAY10s" newPage={true}>
+            github
+          </Button>
         </section>
       </div>
     </dialog>
